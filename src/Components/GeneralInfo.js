45fa0d60e42d89def62a195fbbeaaab17feea321
Hyper Water Dog <omegaflakes@gmail.com>
2021-03-07 16:16:04 -0800

    general info section

--- src/Components/GeneralInfo.js
+++ src/Components/GeneralInfo.js
@@ -1,14 +1,20 @@
-import React,{useState} from 'react'
+import React from 'react'
 import Item from './Items/Item'
 
-function GeneralInfo() {
+function GeneralInfo() {        
+    const indexes = [0,1,2,3];
+    const placeholders = ['First Name','Last Name','example@email.com','123-456-7890'];
+    const names = ['First Name','Last Name','Email','Phone Number'];
+
+    const renderContent = () =>{
+        return indexes.map(ele=>{
+            return <Item placeholder={placeholders[ele]} name={names[ele]}/>
+        })
+    }
+
     return (
         <div>
-            <Item/>
-            <Item/>
-            <Item/>
-            <Item/>
-            
+            {renderContent()}
         </div>
     );
 }
--- src/Components/Items/Item.js
+++ src/Components/Items/Item.js
@@ -1,14 +1,15 @@
 import React,{useState} from 'react'
 import Input from './Input'
 
-function GeneralInfo() {
-    const [info,setInfo] = useState({text:'First Name'});
+function GeneralInfo(props) {
+    const [info,setInfo] = useState({text:props.name});
     const [edit,setEdit] = useState(false);
 
     const addInfo = (newInfo) => {
         console.log(newInfo)
         setEdit(false)
         setInfo(newInfo)
+        setEdit(false)
     }
 
     const editInfo = () => {
@@ -17,7 +18,7 @@ function GeneralInfo() {
 
     const getComponent = () => {
         if(edit){
-            return <Input onSubmit={addInfo} value={info.text} name="firstName" type="text" placeholder="123"/>
+            return <Input onSubmit={addInfo} value={info.text} name="firstName" placeholder={props.placeholder}/>
         }else{
             return <p onClick={editInfo}>{info.text}</p>
         }
